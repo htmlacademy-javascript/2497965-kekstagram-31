654bf03b8394a26262f21ff0ccf693bfd4bc9fcc
Лена <alyona610430@gmail.com>
2024-02-28 20:12:05 +0300

    Избавляемся от конатации

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const DESCRIPTIONS = [
   'На всякий случай, а то вдруг вы забыли, как я выгляжу…',
   'Эта поездка подарила мне столько эмоций и впечатлений! Я до сих пор нахожусь под их волшебным очарованием. Не забуду эти моменты!',
   'Выходные, пожалуйста, не оставляйте меня сейчас!'
-]
+];
 
 const MESSAGES = [
   'Всё отлично!',
@@ -13,7 +13,7 @@ const MESSAGES = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-]
+];
 
 const NAMES = [
   'Новиков Николай',
@@ -26,14 +26,14 @@ const NAMES = [
   'Субботин Савелий',
   'Титова Александра',
   'Петрова Полина'
-]
+];
 
 function getRandomInteger (a, b) {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};
+}
 
 function getIdNumber (a, b) {
   const previousValues = [];
@@ -55,8 +55,8 @@ const generateCommentId = getIdNumber(1, 1000);
 function generateComment() {
   return {
     id: generateCommentId(),
-    avatar: 'img/avatar-' + getRandomInteger(1,6) +'.svg',
-     message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
+    avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
+    message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)],
     name: NAMES[getRandomInteger(0, NAMES.length - 1)],
   };
 }
@@ -67,7 +67,7 @@ const generatePhotoId = getIdNumber(1, 25);
 function generatePost() {
   return {
     id: generatePostId(),
-    url: 'photos/' + generatePhotoId() + '.jpg',
+    url: `photos/${generatePhotoId()}.jpg`,
     description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
     likes: getRandomInteger(15, 200),
     comments: Array.from({length: getRandomInteger(0, 30)}, generateComment)
@@ -75,3 +75,4 @@ function generatePost() {
 }
 
 const posts = Array.from({length:25}, generatePost);
+window.console.log(posts);
