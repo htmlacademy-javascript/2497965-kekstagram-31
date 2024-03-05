47b200829d6e70d3cd76dbd4db66b37c5a96a6a4
Keks <keks@htmlacademy.ru>
2024-03-05 20:47:40 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/create-pictures.js
@@ -0,0 +1,18 @@
+import { posts } from './data';
+
+const picturesTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const pictureListElement = document.querySelector('.pictures');
+const createPicture = posts();
+const pictureListFragment = document.createDocumentFragment();
+
+createPicture.forEach(({url, description, likes, comments}) => {
+  const pictureElement = picturesTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('picture__img').alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureListFragment.appendChild(pictureElement);
+});
+pictureListElement.appendChild(pictureListFragment);
