81a38eaf1755cb54f5e6839bad70bbee16282a92
Лена <alyona610430@gmail.com>
2024-03-05 21:42:31 +0300

    отладка

--- js/create-pictures.js
+++ js/create-pictures.js
@@ -9,6 +9,7 @@ const pictureListFragment = document.createDocumentFragment();
 createPicture.forEach(({url, description, likes, comments}) => {
   const pictureElement = picturesTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
+  console.log(description);
   pictureElement.querySelector('picture__img').alt = description;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
