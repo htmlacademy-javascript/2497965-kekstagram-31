3745e9b98802351df685ba59bd8f15a381602159
Лена <alyona610430@gmail.com>
2024-04-02 21:24:41 +0300

    открывается и закрывается

--- js/form.js
+++ js/form.js
@@ -1,8 +1,29 @@
+import {isEscapeKey} from './util.js';
+
 const uploadFileForm = document.querySelector('#upload-file');
 const renderPhotoForm = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
+const closeButton = renderPhotoForm.querySelector('.img-upload__cancel');
 
-uploadFileForm.addEventListener('click', () => {
+function closeForm() {
+  renderPhotoForm.classList.add('hidden');
+  body.classList.remove('modal-open');
+  closeButton.removeEventListener('click', closeForm);
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
+
+function openForm() {
   renderPhotoForm.classList.remove('hidden');
   body.classList.add('modal-open');
-})
+  closeButton.addEventListener('click', closeForm);
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeForm();
+  }
+}
+
+uploadFileForm.addEventListener('click', openForm);
