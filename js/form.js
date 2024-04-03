7d12a643cc72ccc4b3dac4fb6dff37930c630fe4
Лена <alyona610430@gmail.com>
2024-04-03 22:45:33 +0300

    добавление валидации хештегов

--- js/form.js
+++ js/form.js
@@ -1,11 +1,13 @@
 import {isEscapeKey} from './util.js';
+import {isHashtagValid, errorMessage} from './hashtag-validate.js';
+
 const uploadPhotoForm = document.querySelector('.img-upload__form');
 const uploadFile = uploadPhotoForm.querySelector('#upload-file');
 const renderPhotoForm = uploadPhotoForm.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const closeButton = renderPhotoForm.querySelector('.img-upload__cancel');
-const hashtagForm = uploadPhotoForm.querySelector('.text__hashtags');
-const commentForm = uploadPhotoForm.querySelector('.text__description');
+const hashtagInput = uploadPhotoForm.querySelector('.text__hashtags');
+//const commentInput = uploadPhotoForm.querySelector('.text__description');
 
 function closeForm() {
   renderPhotoForm.classList.add('hidden');
@@ -36,7 +38,5 @@ const pristine = new Pristine(uploadPhotoForm, {
   errorTextParent: '.img-upload__field-wrapper'
 });
 
-pristine.addValidator(hashtagForm, (value) => {
-  /^#[a-zа-яё0-9]{1,19}$/i.test(value);
-}, 'Ошибка');
+pristine.addValidator(hashtagInput, isHashtagValid, errorMessage);
 
