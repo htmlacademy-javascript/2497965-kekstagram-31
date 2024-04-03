5ed22cbb53d1680dac5cff784ce9ae986886a412
Лена <alyona610430@gmail.com>
2024-04-03 22:45:56 +0300

    модуль валидации

---
+++ js/hashtag-validate.js
@@ -0,0 +1,36 @@
+const HASHTAG_MAX_COUNT = 5;
+const HASHTAG_MAX_LENGHT = 20;
+//const HASHTAG_VALID_REG = /^#[a-zа-яё0-9]{1,19}$/i;
+let errorMessage = '';
+
+
+function isHashtagValid(value) {
+  const hashtagArray = value.trim.toLowerCase.split(/\s+/);
+  errorMessage = '';
+  if (hashtagArray.lenght === 0) {
+    return true;
+  };
+  if (hashtagArray.lenght > HASHTAG_MAX_COUNT) {
+    errorMessage = `Нельзя указать больше ${HASHTAG_MAX_COUNT} хэштегов`;
+    return false;
+  }
+  if (hashtagArray.some(hasOnlyNumSign(hashtag))) {
+    errorMessage = "Хеш-тег не может состоять только из одной решётки";
+    return false;
+  };
+  if (hashtagArray.some(checkHastagLength(hashtag))) {
+    errorMessage = `Хеш-тег не может быть длиннее ${HASHTAG_MAX_LENGHT}`;
+    return false;
+  };
+
+}
+
+function hasOnlyNumSign(string) {
+  return string === '#';
+}
+
+function checkHastagLength(string) {
+  return string > 20;
+}
+
+export {isHashtagValid, errorMessage};
