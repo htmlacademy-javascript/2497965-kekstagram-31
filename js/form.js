b7e13c1a1c182ad2404259db65a6b688fbeb12b5
Лена <alyona610430@gmail.com>
2024-04-03 23:23:06 +0300

    /

--- js/form.js
+++ js/form.js
@@ -39,5 +39,5 @@ const pristine = new Pristine(uploadPhotoForm, {
   errorTextParent: 'img-upload__field-wrapper'
 });
 
-pristine.addValidator(hashtagInput, isHashtagValid, errorMessage);
+pristine.addValidator(hashtagInput, isHashtagValid, 'rrorMessage');
 
