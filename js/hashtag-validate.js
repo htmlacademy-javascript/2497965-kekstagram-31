70d8fb52f226302d4a8781f10ba7ddea3ef2104c
Лена <alyona610430@gmail.com>
2024-04-04 23:58:28 +0300

    попытка валидации

--- js/hashtag-validate.js
+++ js/hashtag-validate.js
@@ -1,6 +1,6 @@
 const HASHTAG_MAX_COUNT = 5;
 const HASHTAG_MAX_LENGHT = 20;
-//const HASHTAG_VALID_REG = /^#[a-zа-яё0-9]{1,19}$/i;
+const HASHTAG_VALID_REG = /^#[a-zа-яё0-9]{1,19}$/i;
 let errorMessage = '';
 function returnError() {
   return errorMessage;
@@ -12,28 +12,44 @@ function isHashtagValid(value) {
   if (hashtagString.lenght === 0) {
     return true;
   };
-  const hashtagArray = hashtagString.split(/\s+/);
+  const hashtagArray = hashtagString.split(' ');
   if (hashtagArray.lenght > HASHTAG_MAX_COUNT) {
     errorMessage = `Нельзя указать больше ${HASHTAG_MAX_COUNT} хэштегов`;
     return false;
   }
-  if (hashtagArray.some(hasOnlyNumSign)) {
-    errorMessage = "Хеш-тег не может состоять только из одной решётки";
-    return false;
-  };
-  if (hashtagArray.some(checkHastagLength)) {
-    errorMessage = `Хеш-тег не может быть длиннее ${HASHTAG_MAX_LENGHT}`;
-    return false;
-  };
- return true;
+  hashtagArray.forEach((hashtag) => {
+    if (!hasOnlyNumSign(hashtag)||
+    !checkHastagLength||
+    !hasRightMask||) {
+      return false;
+    };
+    return true;
+  });
+
 }
 
-function hasOnlyNumSign(string) {
-  return string === '#';
+function hasOnlyNumSign(hashtag) {
+  if (hashtag === '#') {
+    errorMessage = "Хэштег не может состоять только из одной решётки";
+    return false
+  };
+  return true;
 }
 
-function checkHastagLength(string) {
-  return string > HASHTAG_MAX_LENGHT;
+function checkHastagLength(hashtag) {
+  if (hashtag.lenght > HASHTAG_MAX_LENGHT) {
+    errorMessage = `Хэштег не может быть длиннее ${HASHTAG_MAX_LENGHT}`;
+    return false;
+  };
+  return true;
+}
+
+function hasRightMask(hashtag) {
+  if (!HASHTAG_VALID_REG.test(hashtag)) {
+    errorMessage = `Хэштег должен начинаться с символа # и состоять из букв и чисел`;
+    return false;
+  };
+  return true;
 }
 
 export {isHashtagValid, returnError};
