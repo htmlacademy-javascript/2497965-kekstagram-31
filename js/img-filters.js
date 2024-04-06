cfc4a9f55bc9320a8a6f37cf7c52b768cb7cc7de
Лена <alyona610430@gmail.com>
2024-04-06 22:34:31 +0300

    +

---
+++ js/img-filters.js
@@ -0,0 +1,11 @@
+const imgPreview = document.querySelector('.img-upload__preview img');
+const effectLevel = document.querySelector('.effect-level__value');
+
+const filterSettings = {
+  none: {
+    range: {
+      min: 0,
+      max: 1
+    }
+  }
+}
