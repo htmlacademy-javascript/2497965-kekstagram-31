68c9bcdb0d3ff99ae951fd3ffaac4cc8d73d2697
Лена <alyona610430@gmail.com>
2024-04-07 00:10:39 +0300

    подключение слайдера

--- js/img-filters.js
+++ js/img-filters.js
@@ -1,11 +1,75 @@
 const imgPreview = document.querySelector('.img-upload__preview img');
 const effectLevel = document.querySelector('.effect-level__value');
+const sliderContainer = document.querySelector('..img-upload__effect-level');
+const slider = sliderContainer.querySelector('.effect-level__slider');
 
 const filterSettings = {
-  none: {
+  chrome: {
     range: {
       min: 0,
       max: 1
-    }
-  }
-}
+    },
+    start: 1,
+    step: 0.1,
+    style: 'glayscale',
+  },
+  sepia: {
+    range: {
+      min: 0,
+      max: 1,
+    },
+  start: 1,
+  step: 0.1,
+  style: 'sepia',
+  },
+  marvin: {
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+    style: 'invert',
+    unit: '%',
+  },
+  phobos: {
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+    style: 'blur',
+    unit: 'px',
+  },
+  heat: {
+    range: {
+      min: 1,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+    style: 'brightness',
+  },
+};
+
+noUiSlider.create(slider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 80,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
