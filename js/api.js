5f2ebd30c73ec72452560cf87996ee1ee1acbdc8
Лена <alyona610430@gmail.com>
2024-04-08 21:46:35 +0300

    загрузка данных с сервера

--- js/api.js
+++ js/api.js
@@ -1,5 +1,33 @@
-fetch('https://31.javascript.htmlacademy.pro/code-and-magick/data')
-  .then((response) => response.json())
-  .then((wizards) => {
-    console.log(wizards);
+const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
+
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+};
+
+const loadData = (route, errorText, method = Method.GET, body = null) =>
+fetch(`${BASE_URL}${route}`, {method, body})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  })
+  .catch(() => {
+    throw new Error(errorText);
   });
+
+const getData = () => loadData(Route.GET, ErrorText.GET_DATA);
+const sendData = (body) => loadData(Route.SEND, ErrorText.SEND_DATA, Method.POST, body);
+
+export {getData, sendData};
