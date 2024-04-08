4396a1f73c30d40d3e9bcbf2db47344f97731328
Лена <alyona610430@gmail.com>
2024-04-08 23:17:11 +0300

    пробелы

--- js/api.js
+++ js/api.js
@@ -16,16 +16,16 @@ const ErrorText = {
 };
 
 const loadData = (route, errorText, method = Method.GET, body = null) =>
-fetch(`${BASE_URL}${route}`, {method, body})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error();
-    }
-    return response.json();
-  })
-  .catch(() => {
-    throw new Error(errorText);
-  });
+  fetch(`${BASE_URL}${route}`, {method, body})
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    })
+    .catch(() => {
+      throw new Error(errorText);
+    });
 
 const getData = () => loadData(Route.GET_DATA, ErrorText.GET_DATA);
 const sendData = (body) => loadData(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
