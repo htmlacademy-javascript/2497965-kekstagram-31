677d560f1c87465daeec44829dcfd1952a6a60f3
Лена <alyona610430@gmail.com>
2024-04-08 21:17:04 +0300

    убрала лишнее

--- js/input-validate.js
+++ js/input-validate.js
@@ -2,10 +2,7 @@ const HASHTAG_MAX_COUNT = 5;
 const HASHTAG_MAX_LENGTH = 20;
 const COMMENT_MAX_LENGTH = 140;
 const HASHTAG_VALID_REG = /^#[a-zа-яё0-9]{1,19}$/i;
-// let errorMessage = '';
-// function returnError() {
-//   return errorMessage;
-// }
+
 function stringToArray (value) {
   return value.toLowerCase().trim().split(/\s+/);
 }
@@ -28,34 +25,6 @@ function checkHashtagLength (value) {
 function isHashtagUnique(value) {
   return !(stringToArray(value).some((item, number, array) => array.includes(item, number + 1)));
 }
-// function isHashtagValid(value) {
-//   const hashtagArray = value.toLowerCase().trim().split(/\s+/);
-//   if (hashtagArray.length > HASHTAG_MAX_COUNT) {
-//     errorMessage = `Нельзя указать больше ${HASHTAG_MAX_COUNT} хэштегов`;
-//     return false;
-//   }
-//   hashtagArray.forEach((hashtag) => {
-//     if (hasOnlyNumSign(hashtag) ||
-//     !checkHastagLength(hashtag) ||
-//     !hasRightMask(hashtag) ||
-//     !isHashtagUnique(hashtag)) {
-//       return false;
-//     }
-//     return true;
-//   });
-// }
-
-// function isHashtagUnique(array) {
-//   if (array.some(isUnique(item, nubmer, array))) {
-//     errorMessage = 'Хэштег должен быть уникален';
-//     return false;
-//   }
-//   return true;
-// }
-
-// function isUnique(item, number, array) {
-//   array.includes(item, number + 1)
-// }
 
 function isCommentValid(value) {
   return value.length <= COMMENT_MAX_LENGTH;
