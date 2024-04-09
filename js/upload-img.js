697355a16f91b1e8ed37be3a6004516412103dcf
Лена <alyona610430@gmail.com>
2024-04-10 02:45:29 +0300

    es lint

--- js/upload-img.js
+++ js/upload-img.js
@@ -11,7 +11,7 @@ function onPhotoChooserChange () {
     photoPreview.src = URL.createObjectURL(file);
     effectsPreviews.forEach((preview) => {
       preview.style.backgroundImage = `url('${photoPreview.src}')`;
-    })
+    });
   }
 }
 
