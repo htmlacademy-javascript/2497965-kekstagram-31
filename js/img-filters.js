0f907783d7d966ae1a65a8a7b2a576c5e407b63b
Лена <alyona610430@gmail.com>
2024-04-09 23:23:57 +0300

    +

--- js/img-filters.js
+++ js/img-filters.js
@@ -6,32 +6,32 @@ const filterDefault = filtersContainer.querySelector('#filter-default');
 const filterRandom = filtersContainer.querySelector('#filter-random');
 const filterDiscussed = filtersContainer.querySelector('#filter-discussed');
 
-function onFilterDefaultClick (photos) {
+function onFilterDefaultClick (photos, btn) {
   createPosts(photos);
-  setActiveFilter(evt);
+  setActiveFilter(btn);
 }
 
-function onFilterRandomClick (photos) {
+function onFilterRandomClick (photos, btn) {
   const pictures = shuffle(photos).slice(0, 10);
   createPosts(pictures);
-  setActiveFilter(evt);
+  setActiveFilter(btn);
 }
 
-function onFilterDiscussedClick (photos) {
+function onFilterDiscussedClick (photos, btn) {
   const pictures = photos.sort((a, b) => b.comments.length - a.comments.length);
   createPosts(pictures);
-  setActiveFilter(evt);
+  setActiveFilter(btn);
 }
 
-function setActiveFilter (evt) {
+function setActiveFilter (btn) {
   const activeFilter = filtersContainer.querySelector('.img-filters__button--active');
   activeFilter.classList.remove('.img-filters__button--active');
-  evt.target.classList.add('.img-filters__button--active');
+  btn.classList.add('.img-filters__button--active');
 }
 
 export function initFliters (photos) {
   filtersContainer.classList.remove('img-filters--inactive');
-  filterDefault.addEventListener('click',() => onFilterDefaultClick(photos));
-  filterRandom.addEventListener('click',() => onFilterRandomClick([...photos]));
-  filterDiscussed.addEventListener('click',() => onFilterDiscussedClick([...photos]));
+  filterDefault.addEventListener('click',() => onFilterDefaultClick(photos, filterDefault));
+  filterRandom.addEventListener('click',() => onFilterRandomClick([...photos], filterRandom));
+  filterDiscussed.addEventListener('click',() => onFilterDiscussedClick([...photos], filterDiscussed));
 }
