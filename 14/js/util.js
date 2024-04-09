b4cd0689df26fc3a2cb0dce28738c7aa52b38933
Keks <keks@htmlacademy.ru>
2024-04-10 00:22:04 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/util.js
@@ -0,0 +1,43 @@
+function getRandomInteger (a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getIdNumber (a, b) {
+  const previousValues = [];
+  return function () {
+    let currentValue = getRandomInteger(a, b);
+    if (previousValues.length >= (b - a + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(a, b);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+function isEscapeKey(evt) {
+  return evt.key === 'Escape';
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function debounce(func, ms) {
+  let timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), ms);
+  };
+}
+
+export {getRandomInteger, getIdNumber, isEscapeKey, shuffle, debounce};
