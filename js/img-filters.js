198efd8ce43d772bae9150efc9b83e5368a44b90
Лена <alyona610430@gmail.com>
2024-04-09 22:37:27 +0300

    random

--- js/img-filters.js
+++ js/img-filters.js
@@ -1,3 +1,5 @@
+import {shuffle} from './util.js';
+
 const filtersContainer = document.querySelector('.img-filters');
 const filterDefault = filtersContainer.querySelector('#filter-default');
 const filterRandom = filtersContainer.querySelector('#filter-random');
@@ -8,7 +10,7 @@ function onFilterDefaultClick (photos) {
 }
 
 function onFilterRandomClick (photos) {
-  console.log('random', photos);
+  console.log('random', shuffle(photos).slice(0, 10));
 }
 
 function onFilterDiscussedClick (photos) {
@@ -17,7 +19,7 @@ function onFilterDiscussedClick (photos) {
 
 export function initFliters (photos) {
   filtersContainer.classList.remove('img-filters--inactive');
-  filterDefault.addEventListener('click',onFilterDefaultClick(photos));
+  filterDefault.addEventListener('click',() => onFilterDefaultClick(photos));
   filterRandom.addEventListener('click',() => onFilterRandomClick(photos));
   filterDiscussed.addEventListener('click',() => onFilterDiscussedClick(photos));
 }
