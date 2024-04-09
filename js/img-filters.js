8d92b27e579caf4cf6ab4b18468322e481ada2ef
Лена <alyona610430@gmail.com>
2024-04-09 22:23:01 +0300

    +

--- js/img-filters.js
+++ js/img-filters.js
@@ -4,20 +4,20 @@ const filterRandom = filtersContainer.querySelector('#filter-random');
 const filterDiscussed = filtersContainer.querySelector('#filter-discussed');
 
 function onFilterDefaultClick (photos) {
-  console.log('default');
+  console.log('default', photos);
 }
 
-function onFilterRandomClick () {
-  console.log(photos);
+function onFilterRandomClick (photos) {
+  console.log('random', photos);
 }
 
-function onFilterDiscussedClick () {
-  console.log(photos);
+function onFilterDiscussedClick (photos) {
+  console.log('discussed', photos);
 }
 
 export function initFliters (photos) {
   filtersContainer.classList.remove('img-filters--inactive');
-  filterDefault.addEventListener('click', onFilterDefaultClick(photos));
-  filterRandom.addEventListener('click', onFilterRandomClick(photos));
-  filterDiscussed.addEventListener('click', onFilterDiscussedClick(photos));
+  filterDefault.addEventListener('click',onFilterDefaultClick(photos));
+  filterRandom.addEventListener('click',() => onFilterRandomClick(photos));
+  filterDiscussed.addEventListener('click',() => onFilterDiscussedClick(photos));
 }
