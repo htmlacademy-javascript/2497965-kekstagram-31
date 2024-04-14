f8a796a44b6f125c475150fb4453d8a68352cbb0
Лена <alyona610430@gmail.com>
2024-04-14 19:24:02 +0300

    добавление фото на превью эффекта

--- js/upload-img.js
+++ js/upload-img.js
@@ -1,6 +1,7 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const photoChooser = document.querySelector('#upload-file');
 const photoPreview = document.querySelector('.img-upload__preview img');
+const effectPreview = document.querySelectorAll('.effects_preview');
 
 function onPhotoChooserChange () {
   const file = photoChooser.files[0];
@@ -8,6 +9,9 @@ function onPhotoChooserChange () {
   const matches = FILE_TYPES.some((item) => fileName.endsWith(item));
   if (matches) {
     photoPreview.src = URL.createObjectURL(file);
+    effectPreview.forEach((preview) => {
+      preview.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+    })
   }
 }
 
