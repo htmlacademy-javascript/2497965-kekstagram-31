7e6c3a94abfe059f34778116680aa2ca3cd6fd53
Лена <alyona610430@gmail.com>
2024-04-14 19:43:55 +0300

    фото на превью

--- js/upload-img.js
+++ js/upload-img.js
@@ -1,7 +1,7 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const photoChooser = document.querySelector('#upload-file');
 const photoPreview = document.querySelector('.img-upload__preview img');
-const effectPreview = document.querySelectorAll('.effects_preview');
+const effectsPreview = document.querySelectorAll('.effects__preview');
 
 function onPhotoChooserChange () {
   const file = photoChooser.files[0];
@@ -9,7 +9,7 @@ function onPhotoChooserChange () {
   const matches = FILE_TYPES.some((item) => fileName.endsWith(item));
   if (matches) {
     photoPreview.src = URL.createObjectURL(file);
-    effectPreview.forEach((preview) => {
+    effectsPreview.forEach((preview) => {
       preview.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
     })
   }
